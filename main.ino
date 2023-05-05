fec1ce4f69d9b54afb5878723b4b8109126c4fd9
yeojhenrie <65714639+yeojhenrie@users.noreply.github.com>
2023-05-05 17:47:30 +0800

    LATEST

--- main.ino
+++ main.ino
@@ -1,53 +1,158 @@
-#include <RH_ASK.h>
+#include <SPI.h>
+#include <MFRC522.h>
 #include <LiquidCrystal_I2C.h>
+#include <RH_ASK.h>
+//RH
+const int BIT_RATE = 500;
+  
+const int RX_PIN = 14, TX_PIN = 0;
+  
+RH_ASK driver(BIT_RATE, RX_PIN, TX_PIN);
+//RH
 
-LiquidCrystal_I2C lcd(0x27,16,2);
+//lcd
+int lcdColumns = 16;
+int lcdRows = 2;
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows); 
+//lcd
 
-const int BIT_RATE = 2000;
+#define RST_PIN         16           // Configurable, see typical pin layout above
+#define SS_PIN          15          // Configurable, see typical pin layout above
 
-const int RX_PIN = 14;
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 
-RH_ASK driver(BIT_RATE, RX_PIN);
-
+//*****************************************************************************************//
 void setup() {
+  //initialize the RH and LCD
+  driver.init();  
   lcd.init();
-  lcd.clear();
   lcd.backlight();
-  Serial.begin(9600);
-  pinMode(2, OUTPUT);
-  pinMode(12, OUTPUT);
-  driver.init();
-  lcd.print("WELCOME!");
-}
-
-void loop() {
-  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
-  uint8_t buflen = sizeof(buf);
-
-  if (driver.recv(buf, &buflen)) {
-    lcd.clear();
-    lcd.setCursor(3,1);
-    lcd.print((char*)buf);
-    lcd.setCursor(3,0);
-    lcd.print("EMERGENCY!");
-
-
-    for (int i = 0; i < buflen; i++)
-      Serial.print((char) buf[i]);
-    Serial.println("");
-    int b=0;
-    while(b<10){
-    delay(500);
-    digitalWrite(2,LOW);
-    digitalWrite(12,HIGH);
-    delay(500);
-    digitalWrite(2,HIGH);
-    digitalWrite(12,LOW);
-    b++;
-    }
   lcd.clear();
-  lcd.print("PUSH BUTTON");
-  lcd.setCursor(0,1);
-  lcd.print("EMERGENCY CALLER");
-  }
+  lcd.setCursor(0, 0);
+  lcd.print("WELCOME!");
+  delay(1000);
+  //
+  Serial.begin(9600);                                           // Initialize serial communications with the PC
+  SPI.begin();                                                  // Init SPI bus
+  mfrc522.PCD_Init();                                              // Init MFRC522 card
+  Serial.println(F("Read personal data on a MIFARE PICC:"));    //shows in serial that it is ready to read
 }
+
+//*****************************************************************************************//
+void loop() {
+  // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+  MFRC522::MIFARE_Key key;
+  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+
+  //some variables we need
+  byte block;
+  byte len;
+  MFRC522::StatusCode status;
+
+  //-------------------------------------------
+
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  Serial.println(F("**Card Detected:**"));
+
+  //-------------------------------------------
+
+  mfrc522.PICC_DumpDetailsToSerial(&(mfrc522.uid)); //dump some details about the card
+
+  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));      //uncomment this to see all blocks in hex
+
+  //-------------------------------------------
+
+  Serial.print(F("Name: "));
+
+  byte buffer1[18];
+
+  block = 4;
+  len = 18;
+
+  //------------------------------------------- GET FIRST NAME
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 4, &key, &(mfrc522.uid)); //line 834 of MFRC522.cpp file
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Authentication failed: "));
+    lcd.clear();
+    lcd.print("Auth failed");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  status = mfrc522.MIFARE_Read(block, buffer1, &len);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Reading failed: "));
+    lcd.clear();
+    lcd.print("Reading failed");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print((char*)buffer1);
+  //PRINT FIRST NAME
+  for (uint8_t i = 0; i < 16; i++)
+  {
+    if (buffer1[i] != 32)
+    {
+      Serial.write(buffer1[i]);
+    }
+  }
+
+ driver.send((uint8_t *)(char*)buffer1, 16);
+  Serial.print(" ");
+
+  //---------------------------------------- GET LAST NAME
+
+  byte buffer2[18];
+  block = 1;
+
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 1, &key, &(mfrc522.uid)); //line 834
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Authentication failed: "));
+    lcd.clear();
+    lcd.print("Auth failed");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  status = mfrc522.MIFARE_Read(block, buffer2, &len);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Reading failed: "));
+    lcd.clear();
+    lcd.print("Reading failed");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  lcd.setCursor(0, 1);
+  lcd.print((char*)buffer2);
+  //PRINT LAST NAME
+  for (uint8_t i = 0; i < 16; i++) {
+    //lcd.print(char(buffer2[i]) );
+    Serial.write(buffer2[i] );
+  }
+
+   driver.send((uint8_t *)(char*)buffer2, 16);
+
+
+  //----------------------------------------
+
+  Serial.println(F("\n**End Reading**\n"));
+  delay(1000);
+  lcd.clear();
+  lcd.print("Tap here"); //just change it to what you want.
+  delay(1000); //change value if you want to read cards faster
+
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
+}
+//*****************************************************************************************//
